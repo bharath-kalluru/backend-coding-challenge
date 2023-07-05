e8bba66ecc981132839ca1d6cbaad86ad22dafd5
bharath-kalluru <122293641+bharath-kalluru@users.noreply.github.com>
2023-07-06 00:29:02 +0530

    updated source files

---
+++ src/local_testing_data.py
@@ -0,0 +1,23 @@
+# cities = [
+#     {
+#         "name": "London, ON, Canada",
+#         "latitude": "42.98339",
+#         "longitude": "-81.23304"
+#     },
+#     {
+#         "name": "London, OH, USA",
+#         "latitude": "39.88645",
+#         "longitude": "-83.44825"
+#     },
+#     {
+#         "name": "London, KY, USA",
+#         "latitude": "37.12898",
+#         "longitude": "-84.08326"
+#     },
+#     {
+#         "name": "Londontowne, MD, USA",
+#         "latitude": "38.93345",
+#         "longitude": "-76.54941"
+#     }
+# ]
+
